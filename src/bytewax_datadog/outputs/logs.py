b1d67847c9557aac003b215febe14923d436d65d
James Ward <james@notjam.es>
2025-03-16 12:34:11 -0400

    style: format the output logs parameters

--- src/bytewax_datadog/outputs/logs.py
+++ src/bytewax_datadog/outputs/logs.py
@@ -69,7 +69,10 @@ class LogSinkPartition(StatelessSinkPartition[CreateLogEntry]):
 
 class LogSink(DynamicSink[CreateLogEntry]):
     def __init__(
-        self, client: ApiClient, default_source: str | None = None, extra_tags: Sequence[str] = tuple()
+        self,
+        client: ApiClient,
+        default_source: str | None = None,
+        extra_tags: Sequence[str] = tuple(),
     ):
         self._client = client
         self._default_source = default_source
@@ -79,7 +82,6 @@ class LogSink(DynamicSink[CreateLogEntry]):
     def from_environment(cls, source: str, extra_tags: Sequence[str] = tuple()) -> Self:
         # The datadog client does some sort of magic to retrieve this
         # from the environment.  I have to assume it's right?
-        # But we'll also allow passing this in.
         configuration = Configuration()
         client = ApiClient(configuration)
 
